ed1331267758867d3554e8bd0d4dc987ef01be14
para29 <65754533+para29@users.noreply.github.com>
2021-02-27 17:54:58 +0000

    Block_Breaker_project

---
+++ Bloc Breaker/Assets/Scripts/Level.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level : MonoBehaviour
+{
+    //parameters
+   [SerializeField] int breakableBlock;
+
+    //cached reference
+    SceneLoader sceneLoader;
+
+    private void Start()
+    {
+        sceneLoader = FindObjectOfType<SceneLoader>();
+    }
+
+    public void CountBlocks()
+    {
+        breakableBlock++;
+    }
+
+    public void BlockDestroyed()
+    {
+        breakableBlock--;
+
+        if (breakableBlock <= 0)
+        {
+            sceneLoader.loadnextScene();
+        }
+
+       
+    }
+
+}
---
+++ Bloc Breaker/Assets/Scripts/Paddle.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    [SerializeField] public float minX = 1f;
+    [SerializeField] public float maxX = 14f;
+    [SerializeField] public float screenWidthInUnits = 16f;
+
+    //cached
+    GameSession autoPlay;
+    Ball ballPosition;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        autoPlay = FindObjectOfType<GameSession>();
+        ballPosition = FindObjectOfType<Ball>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       //float mouseposinUnits = Input.mousePosition.x / Screen.width * screenWidthInUnits;
+        //Vector2 paddlePos = new Vector2(mouseposinUnits, transform.position.y);
+        Vector2 paddlePos = new Vector2(transform.position.x, transform.position.y);
+        paddlePos.x = Mathf.Clamp(GetXPos(), minX, maxX);
+        transform.position = paddlePos; 
+
+    }
+
+    private float GetXPos()
+    {
+        if(autoPlay.IsAutoPlayEnabled())
+        {
+            return ballPosition.transform.position.x;
+        }
+        else
+        {
+            return Input.mousePosition.x / Screen.width * screenWidthInUnits;
+        }
+    }
+
+}
